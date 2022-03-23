3d32c8d9709dff88eb0ad9ed2190b805bba8fdc6
imhp-7 <himanshupant112@gmail.com>
2022-03-23 10:30:23 +0530

    git commit -m "demo2.java"

---
+++ git_demo/demo2.java
@@ -0,0 +1,10 @@
+package git_demo;
+
+import com.sun.jdi.connect.Connector;
+
+public class demo2 {
+    public static void main(String arg[]){
+        System.out.println("Second attempt");
+    }
+}
+
